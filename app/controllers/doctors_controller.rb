722f8b9e155dfbd62b13697c6dd6583be5ba66c0
Jordan Sewell <jrsewell400@gmail.com>
2020-03-02 12:00:57 -0700

    Add final work for US4

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -6,7 +6,10 @@ class DoctorsController < ApplicationController
   def destroy 
     doctor = Doctor.find(params[:id])
     binding.pry
-    doctor.patients.join(:doctor_patients).find(params[:patient_id]).destroy
+    doctor.patients.find(id: params[:id]).destroy
+    # doctor.patients.find_by(patient_id: params[:patient_id], doctor_id: params[:doctor_id]).destroy
+    # patients = doctor.patients
+    # #.find(params[:patient_id]).destroy
     redirect_to doctor_path
   end 
 end 
